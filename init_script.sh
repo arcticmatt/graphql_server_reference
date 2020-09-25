0e1db86672ed1818cb7178361bb7cbaf55d6cfb1
Matt Lim <mlimcollege@gmail.com>
2020-09-24 17:42:07 -0700

    In init_script.sh, change generated script from index.js to index.ts.

--- init_script.sh
+++ init_script.sh
@@ -4,6 +4,6 @@ yarn
 yarn add apollo-server graphql typescript
 yarn add -D eslint ts-node-dev 
 mkdir src
-touch src/index.js
+touch src/index.ts
 touch .gitignore
 echo "node_modules" >> .gitignore
